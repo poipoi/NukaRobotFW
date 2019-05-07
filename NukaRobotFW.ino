267556aea2ad652d3c0658091e613f52da49b164
poipoi <poipoi.11110@gmail.com>
2019-05-07 16:36:56 +0900

    緩慢な動きとピクッとした動きを追加。

--- NukaRobotFW.ino
+++ NukaRobotFW.ino
@@ -13,18 +13,43 @@ void rotateServo(float angle) {
   nukaservo.writeMicroseconds(1800.0 * angle / 180.0 + 500); // サーボを動かすコード
 }
 
+#define MOTION_MODE_FAST  (0)
+#define MOTION_MODE_SLOW  (1)
+
+uint16_t motionMode = MOTION_MODE_SLOW;
 uint32_t tickCountMs;
-uint32_t nextReverseCount = 800;
+uint32_t nextMotionCount = 8000;
 bool isIncreece = false;
+uint32_t flipCount = 1000;
 void tick10ms() {
-  if ((tickCountMs % nextReverseCount) == 0) {
-      isIncreece = !isIncreece;
-      nextReverseCount = random(60, 100) * 10;
-  }
+
+  if (motionMode == MOTION_MODE_FAST) {
+    if ((tickCountMs % nextMotionCount) == 0) {
+      // piku moving
+      rotateServo(random(5, 20));
+    }
   
-  int randVal = random(0, 1000);
-  float newAngle = nowAngle + (isIncreece ? 1 : -1) * randVal * 0.2 / 1000.0;
-  rotateServo(newAngle);
+    if ((tickCountMs % nextMotionCount) == 100) {
+      rotateServo(0);
+      nextMotionCount = random(10, 30) * 1000;
+      motionMode = random(0, 2);
+      flipCount = random(500, 1500);
+    }
+  } else {
+    if ((tickCountMs % nextMotionCount) < 1000) {
+      int randVal = random(0, flipCount);
+      float newAngle = nowAngle + randVal * 1.0 / 1000.0;
+      rotateServo(newAngle);
+    } else if ((tickCountMs % nextMotionCount) < 2000) {
+      int randVal = random(0, 1000);
+      float newAngle = nowAngle + -randVal * 1.0 / 1000.0;
+      rotateServo(newAngle);
+    } else if ((tickCountMs % nextMotionCount) == 2000) {
+      nextMotionCount = random(10, 30) * 1000;
+      motionMode = random(0, 2);
+      flipCount = random(500, 1500);
+    }
+  }
   
   tickCountMs += 10;
 }
@@ -36,6 +61,7 @@ void setup() {
   MsTimer2::set(10, tick10ms); //set timer ms
   MsTimer2::start();
 
+  rotateServo(0);
 }
 
 void loop() {
